d25b708aafb5fd981a086fc8647c406bcd60da4d
j-o-h-n-w-i-c-k <109587352+j-o-h-n-w-i-c-k@users.noreply.github.com>
2022-09-29 12:20:58 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -30,7 +30,23 @@ function Hello(props) {
     </div>
   );
 }
-export default class App extends React.Component {
+export default class Test extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1
+          style={{ color: 'white', backgroundColor: 'grey', borderRadius: 5 }}
+        >
+          {' '}
+          hello gunalan
+        </h1>
+        <p>Start editing to see some magic happen :) </p>
+     
+      </div>
+    );
+  }
+}
+export default class App extends React.Component{
   render() {
     return (
       <div>
@@ -58,75 +74,8 @@ export default class App extends React.Component {
             console.log('clear');
           }}
           label="close it"
-        />
+        /> <Test/>
       </div>
     );
   }
 }
-
-// export default class App extends React.Component
-// {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       name: "elayabharathi",
-//       data_set: [],
-//       loadState: false
-
-//     }
-// }
-//
-//   async componentDidMount() {
-//     let data = await axios.get('https://api.github.com/users/hadley/orgs')
-//     this.setState({data_set: data.data, loadState: true})
-
-//   }
-//   demo = (data) => {
-//       alert(data.name+" "+"dfsdgfs")
-
-//     }
-
-//     render () {
-
-//       const {name} = this.state
-//       if(this.state.loadState)
-//       {
-//       return (
-//         <div>
-//           <h1 style={{color: 'white', backgroundColor: 'grey', borderRadius: 10}} className="ssdf" id="asafc">Hello StackBlitz!</h1>
-//           <p>Start editing to see some magic happen :)</p>
-//           <Hello arr={[1,2,3,4]} name={this.state.name + " || "+name} number={2} onClick={this.demo} label="save it" {...this.state} {...this.props}/>
-// <Hello arr={[1,2,3,4]} name={"yogi"} number={2} onClick={(data) => {
-//             console.log("test")
-//           }} label="close"/>
-//           <Test />
-//           {
-//             (this.state.data_set.length > 0) &&
-//             this.state.data_set.map((data,indx) => {
-//                if(data.login == "RConsortium")
-//               {
-//                 return <div key={indx}>{data.description}
-//                 <h1>adsgfvdsvfsdgvd</h1>
-//                 <img style={{width: 50, height: 50, margin: 10}} src={data.avatar_url}/>
-//                 </div>
-//               }
-//               else
-//               {
-//               return <div key={indx}>{data.description}
-//               <img style={{width: 50, height: 50, margin: 10}} src={data.avatar_url}/>
-//               </div>
-//               }
-//             })
-//           }
-//           {
-//             (this.state.data_set.length <= 0) ? "test":""
-//           }
-//         </div>
-//       );
-//         }
-//         else
-//         {
-//           return <div> loading...</div>
-//         }
-//     }
-// }
